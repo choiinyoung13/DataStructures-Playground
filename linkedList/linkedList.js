6d7901e8d96a2a71768bb7efe84b157c499e45d7
chldls153@naver.com <chldls153@gmail.com>
2024-12-22 20:33:00 +0900

    update: 연결리스트 고도화 (+ tail 추가)

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -1,86 +1,36 @@
 class LinkedList {
-  length = 0
   head = null
+  tail = null
 
   add(value) {
-    if (this.head) {
-      let current = this.head
+    const newNode = new Node(value)
 
-      while (current.next) {
-        current = current.next
-      }
-      current.next = new Node(value)
+    if (!this.head) {
+      this.head = newNode
+      this.tail = newNode
     } else {
-      this.head = new Node(value)
+      this.tail.next = newNode
+      newNode.prev = this.tail
+      this.tail = newNode
     }
-    return this.length++
-  }
-
-  search(index) {
-    let current = this.head
-    let count = 0
-
-    while (count < index) {
-      current = current?.next
-      count++
-    }
-    return current?.value
-  }
-
-  remove(index) {
-    // 인덱스가 유효하지 않으면 undefined 반환
-    if (index < 0 || index >= this.length) {
-      return undefined
-    }
-
-    let current = this.head
-    let prev = null
-
-    // 인덱스가 0인 경우, 헤드를 다음 노드로 변경
-    if (index === 0) {
-      this.head = current.next
-      this.length--
-      return current.value
-    }
-
-    // 인덱스가 0이 아닌 경우, 노드를 순회하며 해당 인덱스의 노드 탐색
-    let count = 0
-    while (count < index) {
-      prev = current
-      current = current.next
-      count++
-    }
-
-    // 이전 노드(prev)의 next를 현재 노드(current)의 next로 변경
-    prev.next = current.next
-
-    // 리스트 길이 감소
-    this.length--
-
-    // 제거된 노드의 값 반환
-    return current.value
   }
 }
 
 class Node {
+  prev = null
   next = null
+
   constructor(value) {
     this.value = value
   }
 }
 
-const li = new LinkedList()
+const list = new LinkedList()
+list.add(1)
+list.add(2)
+list.add(3)
 
-li.add(1)
-li.add(2)
-li.add(3)
-li.add(4)
-
-li.search(0)
-li.search(1)
-li.search(3)
-li.search(5)
-
-li.remove(0)
-li.remove(3)
-li.remove(4)
+console.log(list.head.value) // 1
+console.log(list.tail.value) // 3
+console.log(list.head.next.value) // 2
+console.log(list.tail.prev.value) // 2
